b92fb04424fb9d1f9ea30d61fce46d62f5d66c8e
Pedro Kerkoff <pedrokhenrique2003@gmail.com>
2024-02-21 16:31:32 -0300

    feat: add page about

--- js/script.js
+++ js/script.js
@@ -2,3 +2,21 @@ const cartao = document.querySelector('.cartao-interior');
 cartao.addEventListener('click', function () {
     cartao.classList.toggle('virado');
 });
+
+// About
+var tablinks = document.getElementsByClassName("tabLinks");
+var tabcontents = document.getElementsByClassName("tabContents");
+
+function abrirTab(value){
+    for(var tablink of tablinks){
+        tablink.classList.remove("linkAtivo");
+    }
+    for(var tabcontent of tabcontents){
+        tabcontent.classList.remove("tabAtivo");
+    }
+    event.currentTarget.classList.add("linkAtivo");
+    document.getElementById(value).classList.add("tabAtivo");
+}
+
+
+console.log("Teste")
